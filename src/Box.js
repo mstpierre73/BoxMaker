12caec80f03bbf902b815d54d74e2f336a938be2
Marise St-Pierre <naranjaqc@hotmail.com>
2020-07-21 22:02:25 -0400

    final project

--- src/Box.js
+++ src/Box.js
@@ -10,7 +10,7 @@ class Box extends Component {
                     backgroundColor: this.props.color
                 }}>
                 </div>
-                <button>Submit</button>
+                <button onClick={this.props.removeBox}>Remove</button>
         </div>
         )
     }
--- src/BoxList.js
+++ src/BoxList.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 import Box from './Box';
+import NewBoxForm from './NewBoxForm';
 
 class BoxList extends Component {
     constructor(props){
         super(props);
-        this.state = {boxes: [{ height: 40, width: 10, color: "orange"}]}
+        this.state = {boxes: []}
+        this.create = this.create.bind(this);
+    }
+
+    create (newBox) {
+        this.setState({
+            boxes: [...this.state.boxes, newBox]
+        })
+    }
+
+    remove (id) {
+        this.setState({
+            boxes: [...this.state.boxes.filter(box => box.id !== id)]
+        })
     }
 
     render(){
@@ -13,12 +27,16 @@ class BoxList extends Component {
                 height= {box.height}
                 width= {box.width}
                 color= {box.color}
+                key= {box.id}
+                id= {box.id}
+                removeBox={() => this.remove(box.id)}
             />
         ))
 
         return (
             <div>
                 <h1>Color Box Maker</h1>
+                <NewBoxForm createBox={this.create}/>
                 {boxes}
             </div>
         )
